b4043d90a1a997068afc49c2d2f8fdc4c96c1302
Hemraj-singh-15705 <hemraj15705@gmial.com>
2025-01-11 09:28:03 +0530

    add new mirror code file

--- binary_search_tree/PrintPathRoot2Leaf.java
+++ binary_search_tree/PrintPathRoot2Leaf.java
@@ -62,6 +62,18 @@ public class PrintPathRoot2Leaf {
         printRoot2leaf(root.right, path);
         path.remove(path.size()-1);
     }
+
+    public static boolean isValidBST(Node root, Node min,Node max){
+        if(root == null){
+            return true;
+        }
+        if(min != null && root.data <= min.data){
+            return false;
+        }else if(max != null && root.data >= max.data){
+            return false;
+        }
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
     
     public static void main(String[] args) {
         int values[] = {8,5,3,6,10,11,14};
@@ -75,7 +87,13 @@ public class PrintPathRoot2Leaf {
 
         // printInRange(root, 5, 15);
 
-        printRoot2leaf(root, new ArrayList<>());
+        // printRoot2leaf(root, new ArrayList<>());
+
+        if(isValidBST(root, null, null)){
+            System.out.println("Valid");
+        }else{
+            System.out.println("Not Valid");
+        }
 
     }
 }
