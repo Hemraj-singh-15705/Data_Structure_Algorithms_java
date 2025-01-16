adf4a9947752b683c5cd4adb9de469053b2583e6
Hemraj-singh-15705 <hemraj15705@gmial.com>
2025-01-16 14:42:02 +0530

    add new method for Priority Queue for Object

---
+++ Heaps/PqForObject.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+public class PqForObject {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        
+        public Student(String name, int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("a", 4));
+        pq.add(new Student("b", 6));
+        pq.add(new Student("b", 2));
+        pq.add(new Student("d", 3));
+        pq.add(new Student("e", 1));
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek().name +"->"+pq.peek().rank);
+            pq.remove();
+        }
+    }
+}
--- Heaps/classRoom.java
+++ Heaps/classRoom.java
@@ -1,5 +1,20 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
 public class classRoom{
+    
     public static void main(String[] args) {
-        
+        //Ascending Order 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //Descending Order 
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
     }
 }
