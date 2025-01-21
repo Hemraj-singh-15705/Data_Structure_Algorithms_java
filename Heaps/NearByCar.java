316ee1588bee1352551919cbfd08477fb3ebf5fe
Hemraj-singh-15705 <hemraj15705@gmial.com>
2025-01-21 09:35:11 +0530

    Near Car and Connect n ropes problem

---
+++ Heaps/NearByCar.java
@@ -0,0 +1,35 @@
+import  java.util.*;
+public class NearByCar {
+    static class Point implements Comparable<Point>{
+        int x;
+        int y;
+        int distSeq;
+        int idx;
+
+        public Point(int x,int y,int distSeq,int idx){
+            this.x = x;
+            this.y = y;
+            this.idx =idx;
+            this.distSeq = distSeq;
+
+        }
+
+        @Override
+        public int compareTo(Point p2){
+            return this.distSeq - p2.distSeq;
+        }
+    }
+    public static void main(String[] args) {
+        int pts[][] = {{3,3},{5,-1},{-2,4}};
+        int k = 2;
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        for(int i=0; i<pts.length; i++){
+            int distSeq = pts[i][0]*pts[i][0] + pts[i][1]*pts[i][1];
+            pq.add(new Point(pts[i][0],pts[i][1],distSeq ,i));
+        }
+
+        for(int i=0; i<k; i++){
+            System.out.println("C"+pq.remove().idx);
+        }
+    }
+}
