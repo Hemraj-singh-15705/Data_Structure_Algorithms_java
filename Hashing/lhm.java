748d4f93421116ae144adb17d5ca2c2f77bfa088
Hemraj-singh-15705 <hemraj15705@gmial.com>
2025-01-28 08:16:09 +0530

    add valid anagram check fnx program

--- Hashing/lhm.java
+++ Hashing/lhm.java
@@ -11,7 +11,12 @@ public class lhm {
         lh2.put("Nepal", 50);
         lh2.put("Us", 100);
 
-        System.out.println(lh2);
+        TreeMap<String , Integer> tm = new TreeMap<>();
+        tm.put("india", 150);
+        tm.put("Nepal", 50);
+        tm.put("Us", 100);
+
+        System.out.println(tm);
         System.out.println(lh);
 
     }
