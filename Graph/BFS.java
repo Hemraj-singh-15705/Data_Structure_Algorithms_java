e49ab59db3bf8e9922c87749a84c84afbcbebba2
Hemraj-singh-15705 <hemraj15705@gmial.com>
2025-02-28 09:25:13 +0530

    add new file

--- Graph/BFS.java
+++ Graph/BFS.java
@@ -38,8 +38,16 @@ public class BFS{
         graph[6].add(new Edge(6, 5, 1));
     }
     public static void bfs(ArrayList<Edge>[] graph){
-        Queue<Integer> q = new LinkedList<>();
         boolean vis[] = new boolean[graph.length];
+        for(int i=0; i<graph.length; i++){
+            if(!vis[i]){
+                bfsutil(graph,vis);
+            }
+        }
+    }
+    public static void bfsutil(ArrayList<Edge>[] graph, boolean vis[]){
+        Queue<Integer> q = new LinkedList<>();
+       
         q.add(0); // source 0
 
         while (!q.isEmpty()) {
