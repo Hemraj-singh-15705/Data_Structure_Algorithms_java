e0ee44986fd4f5c152ee561a16e29fbac818fb05
Hemraj-singh-15705 <hemraj15705@gmial.com>
2025-03-02 23:09:40 +0530

    new files

---
+++ Graph/Bipartite.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Bipartite{
+    public static class Edge{
+        int src;
+        int dest;
+        public Edge(int s,int d){
+            this.src=s;
+            this.dest=d;
+        }
+    }
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<>(); //every vertex index store null value 
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+    }
+
+
+    public static boolean isBipartite(ArrayList<Edge>[] graph){
+        int col[] = new int[graph.length];
+        for(int i=0; i<col.length; i++){
+            col[i] = -1; // no color
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0 ; i< graph.length; i++){
+            if(col[i] == -1){
+                
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int V =5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+    }
+}
